683d189af19d930abbf6959115be8b48184bdfe5
mm-zk <mm@matterlabs.dev>
2023-09-14 09:47:58 +0200

    tests

--- src/Counter.sol
+++ src/Counter.sol
@@ -48,3 +48,90 @@ contract CounterTest is Test {
         console.log("value: ", counter.number());
     }
 }
+
+contract ContractBTest is Test {
+    uint256 testNumber;
+
+    function setUp() public {
+        testNumber = 42;
+    }
+
+    function test_NumberIs42() public {
+        assertEq(testNumber, 42);
+    }
+
+    function testFail_Subtract43() public {
+        testNumber -= 43;
+    }
+}
+
+error Unauthorized();
+
+contract OwnerUpOnly {
+    address public immutable owner;
+    uint256 public count;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function increment() external {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
+        count++;
+    }
+}
+
+contract OwnerUpOnlyTest is Test {
+    OwnerUpOnly upOnly;
+
+    function setUp() public {
+        upOnly = new OwnerUpOnly();
+    }
+
+    function test_IncrementAsOwner() public {
+        assertEq(upOnly.count(), 0);
+        upOnly.increment();
+        assertEq(upOnly.count(), 1);
+    }
+    function XtestFail_IncrementAsNotOwner() public {
+        //vm.expectRevert(Unauthorized.selector);
+        //vm.prank(address(0));
+        upOnly.increment();
+    }
+}
+
+
+contract EmitContractTest is Test {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function Xtest_ExpectEmit() public {
+        ExpectEmit emitter = new ExpectEmit();
+        // Check that topic 1, topic 2, and data are the same as the following emitted event.
+        // Checking topic 3 here doesn't matter, because `Transfer` only has 2 indexed topics.
+        vm.expectEmit(true, true, false, true);
+        // The event we expect
+        emit Transfer(address(this), address(1337), 1337);
+        // The event we get
+        emitter.t();
+    }
+
+    function Xtest_ExpectEmit_DoNotCheckData() public {
+        ExpectEmit emitter = new ExpectEmit();
+        // Check topic 1 and topic 2, but do not check data
+        vm.expectEmit(true, true, false, false);
+        // The event we expect
+        emit Transfer(address(this), address(1337), 1338);
+        // The event we get
+        emitter.t();
+    }
+}
+
+contract ExpectEmit {
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function t() public {
+        emit Transfer(msg.sender, address(1337), 1337);
+    }
+}
